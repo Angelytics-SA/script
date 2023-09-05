1b50c30f23fdc24c57b8c16fb027640f68436567
William Brendel <brendel.william@gmail.com>
2023-09-05 01:09:57 -0700

    refactor

--- analytics/record.js
+++ analytics/record.js
@@ -17,13 +17,24 @@ const getMetadata = require('./getMetadata');
 } = {}) => {
   // Get the device data.
   const data = getMetadata(elmt);
-  type && (data.eventType = type);
-  error && (data.error = error);
-  eventName && (data.eventName = eventName);
-  details && typeof details === 'object' && Object.assign(data.details || (data.details = {}), details);
-  body && (data.body = body);
-  (typeof userId === 'number' || userId) && (data.userId = typeof userId === 'object' && JSON.stringify(userId) || `${userId}`)
-  tags && (Array.isArray(tags) || (tags = [tags])) && tags.length && (data.tags = tags);
+  type && (data.event || (data.event = {}), data.event.type = type);
+  error && (data.event || (data.event = {}), data.event.error = error);
+  eventName && (data.event || (data.event = {}), data.event.name = eventName);
+  details && typeof details === 'object'
+    && (
+      data.event || (data.event = {}),
+      Object.assign(data.event.details || (data.event.details = {}), details)
+    );
+  body && (data.event || (data.event = {}), data.event.body = body);
+  (typeof userId === 'number' || userId)
+    && (
+      data.event || (data.event = {}),
+      data.event.userId = typeof userId === 'object'
+        && JSON.stringify(userId) || `${userId}`
+    );
+  tags
+    && (Array.isArray(tags) || (tags = [tags]))
+    && tags.length && (data.event || (data.event = {}), data.event.tags = tags);
 
   // Send data.
   typeof sendFunc === 'function' && sendFunc(data, ect, uri);
